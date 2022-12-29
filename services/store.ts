ff2c216f936d78cf8140903813164d620685bda5
Alexander Gavrilenko <sashadrug@mail.ru>
2022-12-30 00:25:36 +0100

    delete unneeded redux

--- services/store.ts
+++ services/store.ts
@@ -1,13 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import mobileMenuReducer from './headerSlice';
 import buttonReducer from './buttonSlice';
-import teachersReducer from './teachersSlice';
 import telegramReducer from './telegramSlice';
 import modalReducer from './modalSlice';
 import userReducer from './userSlice';
-import reviewsReducer from './reviewsSlice';
-import pricesReducer from './pricesSlice';
-import faqReducer from './faqSlice';
 import locationReducer from './locationSlice';
 import futureEventsReducer from './futureEventsSlice';
 import futureEventDetailsReducer from './futureEventSignUpData';
@@ -17,13 +13,9 @@ export const store = configureStore({
     reducer:{
         mobileMenu: mobileMenuReducer,
         button: buttonReducer,
-        teachers: teachersReducer,
         telegram: telegramReducer,
         modal: modalReducer,
         user: userReducer,
-        reviews: reviewsReducer,
-        prices: pricesReducer,
-        faq: faqReducer,
         location: locationReducer,
         futureEvents: futureEventsReducer,
         futureEventDetails: futureEventDetailsReducer,
