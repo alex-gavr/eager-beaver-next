e6c7a988571cac4673032e7069370beaca680b1b
Alexander Gavrilenko <sashadrug@mail.ru>
2023-01-02 14:06:06 +0100

    load YMetrika lazy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,7 @@ import DayNightToggle from 'react-day-and-night-toggle';
 import dynamic from 'next/dynamic';
 import { Analytics } from '@vercel/analytics/react';
 import { LazyMotion } from 'framer-motion';
+import Script from 'next/script';
 
 const Footer = dynamic(() => import('../components/menus/footer/footer'));
 const FixedSocialMedia = dynamic(() => import('../components/social-media-block/FixedSocialMedia'));
@@ -125,6 +126,32 @@ const EagerBeaverApp = ({ Component, pageProps, router }: AppProps) => {
                 <LazyMotion features={async () => (await import('../components/domAnimation')).default}>
                     <Provider store={store}>
                         <SkeletonTheme baseColor='#cdf0b7' highlightColor='#f8ec9b'>
+                            <Script
+                                strategy='lazyOnload'
+                                id='y-metrika'
+                                dangerouslySetInnerHTML={{
+                                    __html: `(function (m, e, t, r, i, k, a) {
+                                m[i] =
+                                    m[i] ||
+                                    function () {
+                                        (m[i].a = m[i].a || []).push(arguments);
+                                    };
+                                m[i].l = 1 * new Date();
+                                for (var j = 0; j < document.scripts.length; j++) {
+                                    if (document.scripts[j].src === r) {
+                                        return;
+                                    }
+                                }
+                                (k = e.createElement(t)), (a = e.getElementsByTagName(t)[0]), (k.async = 1), (k.src = r), a.parentNode.insertBefore(k, a);
+                            })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
+                            ym(91764388, 'init', {
+                                clickmap: true,
+                                trackLinks: true,
+                                accurateTrackBounce: true,
+                                webvisor: true,
+                            });`,
+                                }}
+                            />
                             <Wrapper className={`${KoskoBold.variable} ${KoskoRegular.variable}`}>
                                 <GlobalStyle />
                                 <Header />
